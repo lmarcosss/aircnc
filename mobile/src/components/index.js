161f3643aa2b97f57574f7a53dfb802761cb0274
Leonardo dos Santos Marcos <leonardomarcos@USERs-iMac.local>
2019-10-09 17:33:26 -0300

    feat: create input component

--- mobile/src/components/index.js
+++ mobile/src/components/index.js
@@ -1,1 +1,2 @@
 export { GOStackButton } from './button/button.go-stack.component'
+export { GoStackInput } from './input/input.go-stack.component'
---
+++ mobile/src/components/input/input.go-stack.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class GoStackInput extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Input </Text>
+            </View>
+        )
+    }
+}
